25ab310d19cf748af3a4a78cd4e8816dc33690d9
bindushishir1 <bindushishir004@gmail.com>
2025-07-02 01:33:41 +0545

    routing changed

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
 import Layout from './component/Layout/Layout.jsx'
-import Project from './component/Project/Project.jsx'
-import Skills from './component/Skill/Skills.jsx'
-import Contact from './component/Contact/Contact.jsx'
-import Home from './component/Home/Home.jsx'
-import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 import Portfolio from './component/Portfolio/Portfolio.jsx'
-const router = createBrowserRouter(
+
+import {
+  Route,
+  createHashRouter,
+  createRoutesFromElements,
+  RouterProvider
+} from 'react-router-dom'
+
+const router = createHashRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Layout/>}>
-      <Route path="" element={<Portfolio/>}/>
-      
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Portfolio />} />
     </Route>
   )
 )
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router ={router}/>
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
