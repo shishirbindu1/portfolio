18773e8d4e38bbcd6148d6780c084a4542a7b01f
bindushishir1 <bindushishir004@gmail.com>
2025-07-05 13:14:07 +0545

    dark light theme is added

--- src/component/Portfolio/Portfolio.jsx
+++ src/component/Portfolio/Portfolio.jsx
@@ -1,34 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Home from "../Home/Home";
 import Project from "../Project/Project";
 import Skills from "../Skill/Skills";
 import Contact from "../Contact/Contact";
-import Header from "../Header/Header"
-import Footer from "../Footer/Footer"
-
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import { ThemeProvider } from "../context/theme";
 
 const Portfolio = () => {
+  const [themeMode, setThemeMode] = useState("light");
+  const darkMode = () => {
+    setThemeMode('dark')
+  };
+  const lightMode = () => {
+    setThemeMode('light')
+  };
+
+  useEffect((e)=>{
+    document.querySelector('html').classList.remove('light','dark')
+    document.querySelector('html').classList.add(themeMode)
+  },[themeMode])
+
   return (
-    <div className="wrapper px-6 grid gap-y-16">
-      <section className="header">
-        <Header />
-      </section>
-      <section id="home">
-        <Home />
-      </section>
-      <section id="project">
-        <Project />
-      </section>
-      <section id="skills">
-        <Skills />
-      </section>
-      <section id="contact">
-        <Contact />
-      </section>
-      <section id="footer">
-        <Footer />
-      </section>
-    </div>
+    <ThemeProvider value={{ themeMode, darkMode, lightMode }}>
+      <div className="wrapper px-6 grid gap-y-16  bg-gray-100 dark:bg-gray-700">
+        <section className="header">
+          <Header />
+        </section>
+        <section id="home">
+          <Home />
+        </section>
+        <section id="project">
+          <Project />
+        </section>
+        <section id="skills">
+          <Skills />
+        </section>
+        <section id="contact">
+          <Contact />
+        </section>
+        <section id="footer">
+          <Footer />
+        </section>
+      </div>
+    </ThemeProvider>
   );
 };
 
