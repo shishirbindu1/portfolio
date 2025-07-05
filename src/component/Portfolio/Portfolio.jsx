dd6116f43c09fd2cf248c41ba16cc4939fd0ce86
bindushishir1 <bindushishir004@gmail.com>
2025-07-05 11:42:16 +0545

    routing is managed

--- src/component/Portfolio/Portfolio.jsx
+++ src/component/Portfolio/Portfolio.jsx
@@ -3,10 +3,16 @@ import Home from "../Home/Home";
 import Project from "../Project/Project";
 import Skills from "../Skill/Skills";
 import Contact from "../Contact/Contact";
+import Header from "../Header/Header"
+import Footer from "../Footer/Footer"
+
 
 const Portfolio = () => {
   return (
     <div className="wrapper px-6 grid gap-y-16">
+      <section className="header">
+        <Header />
+      </section>
       <section id="home">
         <Home />
       </section>
@@ -18,6 +24,9 @@ const Portfolio = () => {
       </section>
       <section id="contact">
         <Contact />
+      </section>
+      <section id="footer">
+        <Footer />
       </section>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-
-import Layout from './component/Layout/Layout.jsx'
 import Portfolio from './component/Portfolio/Portfolio.jsx'
-
-import {
-  Route,
-  createHashRouter,
-  createRoutesFromElements,
-  RouterProvider
-} from 'react-router-dom'
-
-const router = createHashRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route index element={<Portfolio />} />
-    </Route>
-  )
-)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Portfolio/>
   </StrictMode>
 )
