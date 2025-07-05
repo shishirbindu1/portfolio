0f600678fdaac096e8b46f74f8d15c6259f4cd76
bindushishir1 <bindushishir004@gmail.com>
2025-07-05 15:14:22 +0545

    removed extre project

--- src/component/ProjectData/ProjectData.js
+++ src/component/ProjectData/ProjectData.js
@@ -9,14 +9,7 @@ const projects = [
     github: "https://github.com/your-repo",
     live: "https://your-live-site.com",
   },
-  {
-    id: 2,
-    image: projectTrial,
-    title:"dashboard second",
-    technologies: ["Next.js", "Tailwind"],
-    github: "https://github.com/another-repo",
-    live: "https://another-live-site.com",
-  }
+ 
 ];
 
 export default projects
